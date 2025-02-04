216b28ecfc4ff989cf4986cec0ed1b7019d58b82
Ayushman <ayushmanchoudhary43@gmail.com>
2025-02-04 14:28:44 +0530

    updated

--- script.js
+++ script.js
@@ -1,80 +1,104 @@
-let p=document.querySelector("#display-turn");/* This selects a paragraph tag with id display-turn for displaying turns*/
-let p1=document.querySelector("p");//This selects a paragraph tag for displaying choice between X or O
-let btn=document.querySelectorAll(".boxtext");//This selects All button tags 
-let reset=document.getElementById("reset");//This code selects reset button
+let p = document.querySelector("#display-turn"); // Displays current turn
+let p1 = document.querySelector("p"); // Displays player choices
+let btn = document.querySelectorAll(".boxtext"); // Selects all Tic-Tac-Toe buttons
+let reset = document.getElementById("reset"); // Reset button
 
-// selecting X or O 
-let player1=prompt("Choose X or O");
+// Selecting X or O
+let player1 = prompt("Choose X or O");
 let player2;
 
-
-/*This code checks if the user entered X or O and 
-assign player2 value according to the entered choice.
-
-This code also checks and alerts you if you enter any value 
-other than X or O and ask you to enter X or O again*/
-
-do{
-    if(player1=="X"){
-        player2="O";
+do {
+    if (player1 == "X") {
+        player2 = "O";
         break;
-    }
-    else if(player1=="O"){
-        player2="X";
+    } else if (player1 == "O") {
+        player2 = "X";
         break;
+    } else {
+        alert("Please enter X or O");
+        player1 = prompt("Choose X or O");
     }
-    else{
-        alert("please enter between X or O");
-        player1=prompt("Choose X or O");
+} while (player1 != "X" && player1 != "O");
+
+// Display player selections
+p1.innerHTML = `Player1 = ${player1} and Player2 = ${player2}`;
+
+let turn = player1;
+let isgameover = false;
+
+const changeTurn = () => {
+    return turn == "X" ? "O" : "X";
+};
+
+// Function to check if a player has won
+const checkWin = () => {
+    let box = document.getElementsByClassName("boxtext");
+    let win = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
+
+    for (let e of win) {
+        if (box[e[0]].innerText === "O" && box[e[1]].innerText === "O" && box[e[2]].innerText === "O") {
+            box[e[0]].style.backgroundColor = "lightblue";
+            box[e[1]].style.backgroundColor = "lightblue";
+            box[e[2]].style.backgroundColor = "lightblue";
+            p.innerText = (player1 == "O") ? `Player1 (${player1}) won` : `Player2 (${player2}) won`;
+            isgameover = true;
+            return;
+        }
+        else if (box[e[0]].innerText === "X" && box[e[1]].innerText === "X" && box[e[2]].innerText === "X") {
+            box[e[0]].style.backgroundColor = "lightblue";
+            box[e[1]].style.backgroundColor = "lightblue";
+            box[e[2]].style.backgroundColor = "lightblue";
+            p.innerText = (player1 == "X") ? `Player1 (${player1}) won` : `Player2 (${player2}) won`;
+            isgameover = true;
+            return;
+        }
     }
-}while(player1!="X"||player2!="O")
+};
 
+// **Function to check for a draw**
+const checkDraw = () => {
+    let box = document.getElementsByClassName("boxtext");
+    let isFull = true;
+    
+    for (let i = 0; i < box.length; i++) {
+        if (box[i].innerText === "") {
+            isFull = false;
+            break;
+        }
+    }
 
-// This code displays what you entered as player1 and the value player2 gets
-p1.innerHTML=`player1 = ${player1} and player2 = ${player2} `;
+    if (isFull && !isgameover) {
+        p.innerText = "It's a Draw!";
+        isgameover = true;
+    }
+};
 
-let turn=player1;
-const changeTurn=()=>{
-    return turn=="X"?"O":"X";
-}
-const checkWin=()=>{
-    let box=document.getElementsByClassName("boxtext");
-    let win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-    win.forEach(e=>{
-      if(box[e[0]].innerText=="O" && box[e[1]].innerText=="O" && box[e[2]].innerText=="O"){
-        if(player1=="O"){
-            p.innerText=`player1(${player1}) won`;
+// Main game logic
+Array.from(btn).forEach(element => {
+    element.addEventListener("click", () => {
+        if (element.innerText == "" && !isgameover) {
+            element.innerText = turn;
+            checkWin();
+            checkDraw(); // Check for a draw after every move
+            if (!isgameover) {
+                turn = changeTurn();
+                p.innerText = "TURN FOR " + turn;
+            }
+        } else if (element.innerText != "") {
+            p.innerText = "Please reset or click on a remaining empty cell";
         }
-        else{
-            p.innerText=`player2(${player2}) won`;
-        }
-      }
-      else if(box[e[0]].innerText=="X" && box[e[1]].innerText=="X" && box[e[2]].innerText=="X"){
-        if(player1=="X"){
-            p.innerText=`player1(${player1}) won`;
-        }
-        else{
-            p.innerText=`player2(${player2}) won`;
-        }
-      }
-    })
-}
+    });
 
-Array.from(btn).forEach(element=>{
-    element.addEventListener("click",()=>{
-      if(element.innerText==""){
-        element.innerText=turn;
-        turn=changeTurn();
-        p.innerText="TURN FOR "+turn;
-        checkWin();
-      }
-      else if(element.innerText!=""){
-        p.innerText="Please Reset or click on remaining empty cell";
-      }
-      reset.addEventListener("click",()=>{
-         element.innerText="";
-         p.innerText="";
-      })
-      
-    })
-})
+    // Reset the game
+    reset.addEventListener("click", () => {
+        element.innerText = "";
+        element.style.backgroundColor = "";
+        p.innerText = "";
+        isgameover = false;
+        turn = player1; // Reset turn to player1
+    });
+});
